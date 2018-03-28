a86217701010a6990d7fa54d69b7ffcd91390318
Naoki Shinagawa <naoki.shina@gmail.com>
2018-03-28 19:48:15 +0900

    add auto resize textarea

--- app/assets/javascripts/create-card.js
+++ app/assets/javascripts/create-card.js
@@ -1,14 +1,25 @@
 $(document).on('turbolinks:load', function() {
+// todo card html
   var html = `
   <div class = "todo-card">
     <input type="checkbox">
     <input type="date" class="limit-date">
-    <input type="textfield" class="todo-text">
+    <textarea class="todo-text"></textarea>
     <input type="submit" name="" value="追加" class="submit-btn">
-    <i class="fa fa-times fa-lg"></i>
+    <a href="#">
+      <i class="fa fa-times fa-lg"></i>
+    </a>
   </div>`
 
+// create todo card
   $('.todo-btn').on('click', function(e) {
     $('.contents__cover__left').prepend(html);
+// textarea auto resize
+    autosize(document.querySelectorAll('textarea'));
+
+    $('.submit-btn .fa-times').hover(function() {
+      $('.submit-btn, .fa-times').css('cursor', 'pointer');
+      $('.submit-btn, .fa-times').css('opacity', '0.2');
+    });
   });
 });
