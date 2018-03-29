9c94ad65870b29b93c04eac7b51c746f16288c88
Naoki Shinagawa <naoki.shina@gmail.com>
2018-03-29 20:34:24 +0900

    add to create new board btn

--- app/assets/javascripts/user-profile-list.js
+++ app/assets/javascripts/user-profile-list.js
@@ -1,6 +1,12 @@
 $(document).on('turbolinks:load', function() {
+// user profile list hide and show
   $('.header__profile-list').hide();
   $('.user-profile').on('click', function() {
     $('.header__profile-list').toggle(100);
+  });
+// create board list hide and show
+  $('.header__create-board').hide();
+  $('.add-boader').on('click', function() {
+    $('.header__create-board').toggle(100);
   });
 });
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,11 @@
 class TodosController < ApplicationController
+  before_action :variable_sets
   def index
+  end
+
+  private
+  def variable_sets
+    # for create board
+    @board = Board.new
   end
 end
