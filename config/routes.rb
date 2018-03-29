e4dae414a9a1bebd75fb531c69646a032eced153
Naoki Shinagawa <naoki.shina@gmail.com>
2018-03-29 19:55:53 +0900

    use render plate n create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+# board action
+  resources :boards, only: [:new, :create, :edit, :update] do
+# todo action
+    resources :todos, only: [:index]
+  end
 # index
   root 'todos#index'
 end
