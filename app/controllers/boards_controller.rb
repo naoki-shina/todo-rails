5802a64e0242a815e7c76822725edf2cca6a272b
Naoki Shinagawa <naoki.shina@gmail.com>
2018-03-29 20:44:50 +0900

    add action in boards

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,4 +2,13 @@ class BoardsController < ApplicationController
   def new
     @board = Board.new
   end
+
+  def create
+    @board = Board.new(board_params)
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:name)
+  end
 end
