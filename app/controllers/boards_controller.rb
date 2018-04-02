6a7d0dddc89bffab9c638724535b90b20081c779
Naoki Shinagawa <naoki.shina@gmail.com>
2018-04-02 18:09:26 +0900

    add create method in board_controller

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,10 +5,13 @@ class BoardsController < ApplicationController
 
   def create
     @board = Board.new(board_params)
+    @board.save
+    @user_boards = UserBoard.new(user_id: current_user.id, board_id: @board.id)
+    @user_boards.save
   end
 
   private
   def board_params
-    params.require(:board).permit(:name)
+    params.require(:board).permit(:name, { :user_ids => [] })
   end
 end
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,6 @@
 class Board < ApplicationRecord
   has_many :todos
-  has_many :user_boards
+  has_many :user_boards, dependent: :destroy
   has_many :users, through: :user_boards
+  accepts_nested_attributes_for :user_boards
 end
