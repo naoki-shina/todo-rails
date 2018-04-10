0c30dcbd319147b50bafe581a6d4d04df258561b
Naoki Shinagawa <naoki.shina@gmail.com>
2018-04-10 19:53:40 +0900

    create card list view

--- app/assets/javascripts/create-card.js
+++ app/assets/javascripts/create-card.js
@@ -1,10 +1,18 @@
 $(document).on('turbolinks:load', function() {
 // hide post todo card
-  $('.todo-card-new').hide();
-// show post todo card
-  $('.todo-btn').on('click', function(e) {
-    $('.todo-card-new').show();
-// textarea auto resize
-    autosize(document.querySelectorAll('textarea'));
+  $('.js-1, .js-2, .js-3').hide();
+// show post todo card-1
+  $('.list-1').on('click', function(e) {
+    $('.js-1').show();
   });
+// show post todo card-2
+  $('.list-2').on('click', function(e) {
+    $('.js-2').show();
+  });
+// show post todo card-3
+  $('.list-3').on('click', function(e) {
+    $('.js-3').show();
+  });
+// textarea auto resize
+  autosize(document.querySelectorAll('textarea'));
 });
